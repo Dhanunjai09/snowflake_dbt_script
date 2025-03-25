2a452d7c7aff908b6a6d283ad3d6b86cd488fab5
Dhanunjai Seelam <dhanunjai09@gmail.com>
2025-03-25 09:37:47 +0000

    commit

--- models/blinkit_view1.sql
+++ models/blinkit_view1.sql
@@ -7,5 +7,5 @@
 SELECT '$'||TO_CHAR(ROUND(SUM(SALES)/1000000,2),'9999999.99')||'M' TOTAL_SALES,
        ROUND(AVG(SALES)) AVG_SALES,     
        COUNT(ITEM_IDENTIFIER) NO_OF_ITEMS,
-       ROUND(AVG(RATING)) AVG_RATING
+       ROUND(AVG(RATING)) AVG_ITEM_RATING
 FROM {{ source('src_blinkit', 'blinkit_grocery_data_dev_t') }}
